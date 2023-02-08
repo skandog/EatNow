3bf5a6639b3fe4d868299de11199cb8fd1c2a0cf
skandog <muumrar@gmail.com>
2023-02-08 16:41:49 +0000

    refactorred the imp of statusbar padding

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import { SafeAreaView, StyleSheet, StatusBar, Platform } from "react-native";
+import { SafeAreaView, StatusBar } from "react-native";
 import styled from "styled-components";
 
 import { RestaurantScreen } from "./src/screens/restaurants.screen";
@@ -8,26 +8,21 @@ import { RestaurantScreen } from "./src/screens/restaurants.screen";
 const SafeArea = styled(SafeAreaView)`
   flex: 1;
   background-color: "#fff";
-  margin-top: ${(isAndroid) => (isAndroid ? StatusBar.currentHeight : 0)}px;
+  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
 `;
 
-const isAndroid = Platform.OS === "android";
+// Previous implementation of padding top - not sure about
+// change, so leaving in case this is more semantic
+// const isAndroid = Platform.OS === "android";
+// margin-top: ${({ isAndroid }) => (isAndroid ? StatusBar.currentHeight : 0)}px;
+
 export default function App() {
   return (
     <>
       <SafeArea>
         <RestaurantScreen />
       </SafeArea>
-
       <ExpoStatusBar style="auto" />
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    marginTop: isAndroid ? StatusBar.currentHeight : 0,
-  },
-});
