39570675d0ee6ab5edd25eb1235dab8db2fe3c21
skandog <muumrar@gmail.com>
2023-02-09 12:04:14 +0000

    optimised the spacer component to use theme, and adressed bug on android

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -81,9 +81,9 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
                 CLOSED TEMPORARILY
               </Text>
             )}
-            <Spacer variant={"left.large"} />
-            {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
-            <Spacer variant={"left.large"} />
+            <Spacer position="left" size="large">
+              {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
+            </Spacer>
 
             <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
           </SectionEnd>
