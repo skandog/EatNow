a01be543b9eebc19915fa043f10a99ecd931f927
skandog <muumrar@gmail.com>
2023-02-09 10:17:43 +0000

    added address field

--- src/components/restaurant-info-card.component.js
+++ src/components/restaurant-info-card.component.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Text, Card } from "react-native-paper";
+import { View } from "react-native";
 import styled from "styled-components/native";
 
 const RestaurantCard = styled(Card)`
@@ -11,11 +12,19 @@ const RestaurantCardCover = styled(Card.Cover)`
   background-color: ${(props) => props.theme.colors.bg.primary};
 `;
 
+const RestaurantCardInfo = styled(View)`
+  padding: ${(props) => props.theme.space.l};
+`;
+
 const RestaurantCardTitle = styled(Text)`
   font-family: ${(props) => props.theme.fonts.heading};
   font-size: ${(props) => props.theme.fontSizes.title};
-  padding: ${(props) => props.theme.space.l};
   color: ${(props) => props.theme.colors.text.primary};
+`;
+
+const RestaurantCardAddress = styled(Text)`
+  font-family: ${(props) => props.theme.fonts.body};
+  font-size: ${(props) => props.theme.fontSizes.body};
 `;
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
@@ -25,7 +34,7 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
     photos = [
       "https://res.cloudinary.com/grubhub/image/upload/d_search:browse-images:default.jpg/w_267,q_auto:low,fl_lossy,dpr_2.0,c_fill,f_auto,h_132,g_auto/s1rpyrpvzgnww1jmxus1",
     ],
-    address = "100 london street",
+    address = "100 london street, London, SW1H 9HH",
     isOpenNow = true,
     rating = 4,
     isClosedTemporarily,
@@ -34,7 +43,10 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
   return (
     <RestaurantCard elevation={5}>
       <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
-      <RestaurantCardTitle>{name}</RestaurantCardTitle>
+      <RestaurantCardInfo>
+        <RestaurantCardTitle>{name}</RestaurantCardTitle>
+        <RestaurantCardAddress>{address}</RestaurantCardAddress>
+      </RestaurantCardInfo>
     </RestaurantCard>
   );
 };
