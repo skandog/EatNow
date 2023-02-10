acb66b3dd2d37e84269742e629ed7fcb2f34d33c
skandog <muumrar@gmail.com>
2023-02-10 14:47:38 +0000

    added spacer to renderItem for better readability

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -3,6 +3,7 @@ import { View, FlatList } from "react-native";
 import { Searchbar } from "react-native-paper";
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 import styled from "styled-components";
+import { Spacer } from "../../../components/spacer/spacer.component";
 
 const SearchView = styled(View)`
   background-color: ${(props) => props.theme.colors.brand.primary};
@@ -22,8 +23,12 @@ export const RestaurantScreen = () => {
         <Searchbar />
       </SearchView>
       <FlatList
-        data={[{ name: 1 }, { name: 2 },]}
-        renderItem={() => <RestaurantInfoCard />}
+        data={[{ name: 1 }, { name: 2 }, { name: 3 }, { name: 4 }]}
+        renderItem={() => (
+          <Spacer position={"bottom"} size="large">
+            <RestaurantInfoCard />
+          </Spacer>
+        )}
         keyExtractor={(item) => item.name}
         contentContainerStyle={{ padding: 16 }}
       />
