9e9cff5922b056dde9ac190c5294c17fe028d75b
skandog <muumrar@gmail.com>
2023-02-13 17:11:57 +0000

    set up state in context provider, as well as retrieve mock data

--- src/services/restaurants/restaurants.context.js
+++ src/services/restaurants/restaurants.context.js
@@ -1,9 +1,35 @@
 import React, { useState, createContext, useEffect, useMemo } from "react";
 
-import { restaurantsRequest, restaurantTransform } from "./restaurants.service";
+import {
+  restaurantsRequest,
+  restaurantsTransform,
+} from "./restaurants.service";
 
 export const RestaurantContext = createContext();
 
 export const RestaurantContextProvider = ({ children }) => {
+  const [restaurants, setRestaurants] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const retrieveRestaurants = () => {
+    setIsLoading(true);
+    setTimeout(() => {
+      restaurantsRequest()
+        .then(restaurantsTransform)
+        .then((results) => {
+          setIsLoading(false);
+          setRestaurants(results);
+        })
+        .catch((err) => {
+          setIsLoading(false);
+          setError(err);
+        });
+    }, 2000);
+  };
+  useEffect(() => {
+    retrieveRestaurants();
+  }, []);
+
   return <RestaurantContext.Provider>{children}</RestaurantContext.Provider>;
 };
