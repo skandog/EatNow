246f2e0784bfbffca0a91147dd22da9bac4f17b0
skandog <muumrar@gmail.com>
2023-02-16 15:16:07 +0000

    added scroll view to accordian, needed to add flex: 1 to container

--- src/features/restaurants/screens/restaurant-detail.screen.js
+++ src/features/restaurants/screens/restaurant-detail.screen.js
@@ -1,59 +1,74 @@
 import React, { useState } from "react";
+import { ScrollView } from "react-native";
 import { List } from "react-native-paper";
+import { Spacer } from "../../../components/spacer/spacer.component";
 
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 import { ModalContainer } from "../components/restaurant-info-card.styles";
 
 export const RestaurantDetailScreen = ({ route, navigation }) => {
-  const [expanded, setExpanded] = React.useState(true);
+  const [expanded, setExpanded] = useState(true);
 
   const handlePress = () => setExpanded(!expanded);
 
   const { restaurant } = route.params;
   return (
     <ModalContainer>
-      <RestaurantInfoCard restaurant={restaurant} />
-      <List.Section>
-        <List.Accordion
-          title="Breakfast"
-          left={(props) => <List.Icon {...props} icon="coffee" />}
-          expanded={expanded}
-          onPress={handlePress}
-        >
-          <List.Item title="Bacon" />
-          <List.Item title="Eggs" />
-          <List.Item title="Coffee" />
-        </List.Accordion>
+      <Spacer position={"top"} size={"medium"}>
+        <RestaurantInfoCard restaurant={restaurant} />
+      </Spacer>
+      <ScrollView>
+        <List.Section>
+          <List.Accordion
+            title="Breakfast"
+            left={(props) => <List.Icon {...props} icon="bread-slice" />}
+            expanded={expanded}
+            onPress={handlePress}
+          >
+            <List.Item
+              title="Sausages"
+              left={(props) => <List.Icon {...props} icon="sausage" />}
+            />
+            <List.Item
+              title="Eggs"
+              left={(props) => <List.Icon {...props} icon="egg-fried" />}
+            />
+            <List.Item
+              title="Coffee"
+              left={(props) => <List.Icon {...props} icon="coffee" />}
+            />
+          </List.Accordion>
 
-        <List.Accordion
-          title="Lunch"
-          left={(props) => <List.Icon {...props} icon="food" />}
-        >
-          <List.Item
-            title="Burger"
-            left={(props) => <List.Icon {...props} icon="hamburger" />}
-          />
-          <List.Item
-            title="Fries"
-            left={(props) => <List.Icon {...props} icon="french-fries" />}
-          />
-        </List.Accordion>
-        <List.Accordion
-          title="Dinner"
-          left={(props) => (
-            <List.Icon {...props} icon="silverware-fork-knife" />
-          )}
-        >
-          <List.Item
-            title="Steak"
-            left={(props) => <List.Icon {...props} icon="food-steak" />}
-          />
-          <List.Item
-            title="Chicken Caesar"
-            left={(props) => <List.Icon {...props} icon="food-drumstick" />}
-          />
-        </List.Accordion>
-      </List.Section>
+          <List.Accordion
+            title="Lunch"
+            left={(props) => <List.Icon {...props} icon="food" />}
+          >
+            <List.Item
+              title="Burger"
+              left={(props) => <List.Icon {...props} icon="hamburger" />}
+            />
+            <List.Item
+              title="Fries"
+              left={(props) => <List.Icon {...props} icon="french-fries" />}
+            />
+          </List.Accordion>
+          <List.Accordion
+            title="Dinner"
+            left={(props) => (
+              <List.Icon {...props} icon="silverware-fork-knife" />
+            )}
+          >
+            <List.Item
+              title="Steak"
+              left={(props) => <List.Icon {...props} icon="food-steak" />}
+            />
+            <List.Item
+              title="Chicken Caesar"
+              left={(props) => <List.Icon {...props} icon="food-drumstick" />}
+            />
+          </List.Accordion>
+        </List.Section>
+      </ScrollView>
     </ModalContainer>
   );
 };
