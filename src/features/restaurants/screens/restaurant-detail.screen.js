0e7e2a34304912dccfc6bb232c6c9e31213081a6
skandog <muumrar@gmail.com>
2023-02-16 15:01:28 +0000

    built out accordian list for menu

--- src/features/restaurants/screens/restaurant-detail.screen.js
+++ src/features/restaurants/screens/restaurant-detail.screen.js
@@ -1,14 +1,50 @@
-import React from "react";
-import { Spacer } from "../../../components/spacer/spacer.component";
+import React, { useState } from "react";
+import { List } from "react-native-paper";
 
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 import { ModalContainer } from "../components/restaurant-info-card.styles";
 
 export const RestaurantDetailScreen = ({ route, navigation }) => {
+  const [expanded, setExpanded] = React.useState(true);
+
+  const handlePress = () => setExpanded(!expanded);
+
   const { restaurant } = route.params;
   return (
     <ModalContainer>
       <RestaurantInfoCard restaurant={restaurant} />
+      <List.Section>
+        <List.Accordion
+          title="Breakfast"
+          left={(props) => <List.Icon {...props} icon="coffee" />}
+          expanded={expanded}
+          onPress={handlePress}
+        >
+          <List.Item title="Bacon" />
+          <List.Item title="Eggs" />
+          <List.Item title="Coffee" />
+        </List.Accordion>
+
+        <List.Accordion
+          title="Lunch"
+          left={(props) => <List.Icon {...props} icon="food" />}
+        >
+          <List.Item title="Burger" />
+          <List.Item title="Fries" />
+        </List.Accordion>
+        <List.Accordion
+          title="Dinner"
+          left={(props) => (
+            <List.Icon {...props} icon="silverware-fork-knife" />
+          )}
+        >
+          <List.Item
+            title="Steak"
+            left={(props) => <List.Icon {...props} icon="food-steak" />}
+          />
+          <List.Item title="Chicken Caesar" />
+        </List.Accordion>
+      </List.Section>
     </ModalContainer>
   );
 };
