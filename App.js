d19843047264fe72115406bcb1024ad61f038481
skandog <muumrar@gmail.com>
2023-02-21 15:06:57 +0000

    setup basic implementation of login with firebase on app level. need to clear. set up authentication context

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import { ThemeProvider } from "styled-components/native";
 import {
@@ -6,6 +6,8 @@ import {
   Oswald_400Regular,
 } from "@expo-google-fonts/oswald";
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 import { theme } from "./src/infrastructure/theme";
 import { Navigation } from "./src/infrastructure/navigation";
@@ -14,7 +16,34 @@ import { RestaurantContextProvider } from "./src/services/restaurants/restaurant
 import { LocationContextProvider } from "./src/services/location/location.context";
 import { FavouritesContextProvider } from "./src/services/favourites/favourites.context";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyB6mjWCgu_6IDJ0UjgLhTyVU02HJyl4quI",
+  authDomain: "eatnow-d8623.firebaseapp.com",
+  projectId: "eatnow-d8623",
+  storageBucket: "eatnow-d8623.appspot.com",
+  messagingSenderId: "853907676167",
+  appId: "1:853907676167:web:9ddab531b3537fc741dea2",
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth();
+
 export default function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  useEffect(() => {
+    signInWithEmailAndPassword(auth, "test@test.com", "testytest")
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      });
+  }, []);
+
   const [oswaldLoaded] = useOswald({
     Oswald_400Regular,
   });
