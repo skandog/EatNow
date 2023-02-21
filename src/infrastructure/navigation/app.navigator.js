afd2289ca109448405febc96b5ead6eb7ed65577
skandog <muumrar@gmail.com>
2023-02-21 16:04:12 +0000

    set up structure for account screens

--- src/infrastructure/navigation/app.navigator.js
+++ src/infrastructure/navigation/app.navigator.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Ionicons } from "@expo/vector-icons";
 import { Text, View } from "react-native";
--- src/infrastructure/navigation/index.js
+++ src/infrastructure/navigation/index.js
@@ -3,13 +3,14 @@ import { AppNavigator } from "./app.navigator";
 import { NavigationContainer } from "@react-navigation/native";
 
 import { AuthenticationContext } from "../../services/authentication/authentication.context";
+import { AccountNavigator } from "./account.navigator";
 
 export const Navigation = () => {
   const { isAuthenticated } = useContext(AuthenticationContext);
 
   return (
     <NavigationContainer>
-      {isAuthenticated ? <AppNavigator /> : <AccountNavigator />}
+      {!isAuthenticated ? <AppNavigator /> : <AccountNavigator />}
     </NavigationContainer>
   );
 };
