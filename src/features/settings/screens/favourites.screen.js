a77024a091606a7dc56199762276189630608f35
skandog <muumrar@gmail.com>
2023-02-27 15:09:34 +0000

    repurposed restaurantList from restaurant screen to favourites screen

--- src/features/settings/screens/favourites.screen.js
+++ src/features/settings/screens/favourites.screen.js
@@ -1,3 +1,36 @@
-import React from "react";
+import React, { useContext } from "react";
+import styled from "styled-components";
+import { TouchableOpacity, FlatList } from "react-native";
 
-export const FavouritesScreen = () => null;
+import { Spacer } from "../../../components/spacer/spacer.component";
+import { RestaurantInfoCard } from "../../restaurants/components/restaurant-info-card.component";
+
+import { FavouritesContext } from "../../../services/favourites/favourites.context";
+
+const RestaurantList = styled(FlatList).attrs({
+  contentContainerStyle: {
+    padding: 16,
+  },
+})``;
+
+export const FavouritesScreen = ({ navigation }) => {
+  const { favourites } = useContext(FavouritesContext);
+
+  return (
+    <RestaurantList
+      data={favourites}
+      renderItem={({ item }) => (
+        <Spacer position={"bottom"} size="large">
+          <TouchableOpacity
+            onPress={() =>
+              navigation.navigate("RestaurantDetail", { restaurant: item })
+            }
+          >
+            <RestaurantInfoCard restaurant={item} />
+          </TouchableOpacity>
+        </Spacer>
+      )}
+      keyExtractor={(item) => item.name}
+    />
+  );
+};
