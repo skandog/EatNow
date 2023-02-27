c553ec75a469d6faec0dc7048854244fd7b95d3c
skandog <muumrar@gmail.com>
2023-02-27 14:58:30 +0000

    created styled component for list item

--- src/features/settings/screens/settings.screen.js
+++ src/features/settings/screens/settings.screen.js
@@ -1,24 +1,28 @@
 import React, { useContext } from "react";
-import { Text, View } from "react-native";
+import styled from "styled-components";
 import { List } from "react-native-paper";
 
 import { SafeArea } from "../../../components/utils/safe-area.component";
 import { AuthenticationContext } from "../../../services/authentication/authentication.context";
 import { AuthButton } from "../../account/components/account.styles";
 
-export const SettingsScreen = () => {
+const SettingsItem = styled(List.Item)`
+  padding: ${(props) => props.theme.space.l};
+`;
+
+export const SettingsScreen = ({ navigation }) => {
   const { onLogout } = useContext(AuthenticationContext);
   return (
     <SafeArea>
       <List.Section>
-        <List.Item
+        <SettingsItem
           style={{ padding: 16 }}
           title="Favourites"
           description="View your favourites"
           left={(props) => <List.Icon {...props} color="black" icon="heart" />}
           onPress={() => navigation.navigate("Favourites")}
         />
-        <List.Item
+        <SettingsItem
           style={{ padding: 16 }}
           title="Logout"
           left={(props) => <List.Icon {...props} color="black" icon="door" />}
